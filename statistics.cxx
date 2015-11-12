f9472d1e55526354104201a3dc3fbb5ba8288f5d
namac100 <nadine.macedo.biniossek@hhu.de>
2015-11-12 22:21:15 +0100

    Homework 3

--- statistics.cxx
+++ statistics.cxx
@@ -3,19 +3,55 @@
 #include <cstdlib>
 
 using namespace std;
-//--------------------------------------------------
-// plenty of lines here
-//--------------------------------------------------
+
+void randnum(double* p, const int N);
+void print(double* p, const int N);
+double meannum(double* p, const int N);
+double varnum(double* p, const int N);
 
 int main(){
    const int N = 100;
    double p[N];
    double mean, var;
+   
+   srand(time(NULL));
 
-   // Some lines here....
+   randnum(p,N);
+   //print(p,N);
+   mean = meannum(p,N);
+   var = varnum(p,N);
 
    cout << "Mean = " << mean << endl;
    cout << "Variance = " << var << endl;
 
    return 0;
+}
+
+void randnum(double* p, const int N){
+  for(int i=1; i<N; i++){
+    p[i] = rand() / double(RAND_MAX); 
+  }
+}
+
+void print(double* p, const int N){
+  for(int i=1; i<N; i++){
+    cout << p[i] << endl;
+  }
+}
+
+double meannum(double* p, const int N){
+  double m = 0.0;
+  for(int i=1; i<N; i++){
+     m += p[i];
+  }
+  return m /= N;
+}
+
+double varnum(double* p, const int N){
+  double v = 0.0;
+  double m = meannum(p, N);
+  for(int i=1; i<N; i++){
+     v += pow((p[i]-m),2.0);
+  }
+  return v /= N;
 }
